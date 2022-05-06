d4c67b6fab4716e08968b93a753fabe6c2a61b93
chuwg <gi828282@naver.com>
2022-05-06 15:42:43 +0900

    500번 오류 -> 404 NotFound 변경

--- src/main/java/com/tistory/roobistory/chuyew/user/userNotFoundException.java
+++ src/main/java/com/tistory/roobistory/chuyew/user/userNotFoundException.java
@@ -1,5 +1,12 @@
 package com.tistory.roobistory.chuyew.user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// 2XX -> OK
+// 4XX -> client
+// 5XX -> server
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class userNotFoundException extends RuntimeException {
     public userNotFoundException(String message) {
         super(message);
